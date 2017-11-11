0785cdc8d10e7810d61fae4557a7a84b61cfc942
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-11 17:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function scuberGreetingForFeet(feet) {
     return "I will gladly take your thirty bucks."
   } else if (feet > 2500) {
     return "No can do."
-  } else {
-    break;
-  }
+  } 
 }
 
 function ternaryCheckCity(city) {
